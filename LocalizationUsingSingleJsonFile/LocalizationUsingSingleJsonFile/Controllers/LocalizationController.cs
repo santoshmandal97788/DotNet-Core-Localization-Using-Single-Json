b6d03027418045661877f3eaa2f235687a521974
santosh mandal <santosh.mandal@ekbana.info>
2023-09-29 10:11:40 +0545

    Localization controller added to test localization

---
+++ LocalizationUsingSingleJsonFile/LocalizationUsingSingleJsonFile/Controllers/LocalizationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace LocalizationUsingSingleJsonFile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocalizationController : ControllerBase
+    {
+        private readonly IStringLocalizer _localizer;
+        public LocalizationController(IStringLocalizer localizer)
+        {
+                _localizer = localizer;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var message = _localizer["nkcpykswzb"].Value;
+            return Ok(message);
+        }
+    }
+}
