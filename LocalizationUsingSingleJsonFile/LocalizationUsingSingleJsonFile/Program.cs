459829e083548d830c2e30563dfb87dde8db7988
santosh mandal <santosh.mandal@ekbana.info>
2023-09-29 10:04:53 +0545

    configuration added for localization in program.cs file and resource file added in json

--- LocalizationUsingSingleJsonFile/LocalizationUsingSingleJsonFile/Program.cs
+++ LocalizationUsingSingleJsonFile/LocalizationUsingSingleJsonFile/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -19,6 +25,19 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+// Configure localization options
+// Configure default culture and supported cultures
+var supportedCultures = new[] { "en-US", "es-ES", "fr-FR" }; // Add supported cultures as needed
+var defaultCulture = new CultureInfo("en-US"); // Set your default culture here
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(defaultCulture),
+    SupportedCultures = supportedCultures.Select(culture => new CultureInfo(culture)).ToList(),
+    SupportedUICultures = supportedCultures.Select(culture => new CultureInfo(culture)).ToList()
+};
+
+app.UseRequestLocalization(localizationOptions);
+
 
 app.MapControllers();
 
